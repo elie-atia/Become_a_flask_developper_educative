33bf647c481a663c0e0a42dc4893cfac661f4c28
elie-atia <elie-atia@hotmail.fr>
2023-01-05 13:13:45 +0200

    create my first flask application

---
+++ first-flask-application/app.py
@@ -0,0 +1,20 @@
+"""Flask Application for Paws Rescue Center."""
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+    """View function for Home Page."""
+    return "Paws Rescue Center 🐾"
+
+@app.route("/about")
+def about():
+    """View function for About Page."""
+    return """We are a non-profit organization working as an animal rescue center. 
+    We aim to help you connect with the purrfect furbaby for you! 
+    The animals you find at our website are rescue animals which have been rehabilitated. 
+    Our mission is to promote the ideology of "Adopt, don't Shop"! """
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=3000)
